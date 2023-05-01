8b6a646e75e92006e6e0de459270b1f37b8bd83e
heerucan <iwpow0307@gmail.com>
2023-05-01 21:57:54 +0900

    feat: WriteView UI 구성 #6

--- Kevin/Kevin/Presentation/Write/View/WriteViewController.swift
+++ Kevin/Kevin/Presentation/Write/View/WriteViewController.swift
@@ -17,6 +17,23 @@ final class WriteViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private let viewModel: WriteViewModel
     
+    private let naviBar = KevinNavigationBar(type: .write)
+    private let dateLabel = UILabel().then {
+        $0.text = "2022년 4월 22일"
+        $0.font = .kevinFont(type: .medium14)
+        $0.textColor = .black
+        $0.textAlignment = .left
+    }
+    
+    private let textView = UITextView().then {
+        $0.font = .kevinFont(type: .regular16)
+        $0.textColor = .black
+        $0.textAlignment = .left
+        $0.text = StringLiteral.placeholder
+    }
+    
+    private let cardButton = KevinButton(type: .card)
+    
     init(viewModel: WriteViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -28,10 +45,44 @@ final class WriteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
         bind()
     }
     
     private func bind() {
         
+    }
+}
+
+extension WriteViewController {
+    private func setUI() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setLayout() {
+        view.addSubviews([naviBar, dateLabel, textView, cardButton])
+        
+        naviBar.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(49)
+            make.directionalHorizontalEdges.equalToSuperview()
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(naviBar.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        textView.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview().inset(14)
+            make.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(450)
+        }
+        
+        cardButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(105)
+            make.centerX.equalToSuperview()
+        }
     }
 }
