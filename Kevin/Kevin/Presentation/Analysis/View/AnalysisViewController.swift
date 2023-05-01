cac3b3854a4a834d284f0a8958d5689406989ebe
heerucan <iwpow0307@gmail.com>
2023-05-01 22:50:24 +0900

    feat: AnalysisView 대략적인 UI 구성 #7

--- Kevin/Kevin/Presentation/Analysis/View/AnalysisViewController.swift
+++ Kevin/Kevin/Presentation/Analysis/View/AnalysisViewController.swift
@@ -17,6 +17,15 @@ final class AnalysisViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private let viewModel: AnalysisViewModel
     
+    private var type: AnalysisType!
+    
+    private let naviBar = KevinNavigationBar(type: .analysis)
+    private let backView = UIImageView()
+    private let anoterButton = UIButton()
+    private let cardView = KevinCardView(type: .neutral)
+    private let shareButton = KevinButton(type: .share)
+    private let resultButton = KevinButton(type: .result)
+    
     init(viewModel: AnalysisViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -28,10 +37,57 @@ final class AnalysisViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
         bind()
     }
     
     private func bind() {
         
+    }
+}
+
+extension AnalysisViewController {
+    private func setUI() {
+        view.backgroundColor = .systemBackground
+        backView.contentMode = .scaleAspectFill
+        type = .neutral
+        backView.image = type.back
+        resultButton.backgroundColor = type.color
+    }
+    
+    private func setLayout() {
+        view.addSubviews([backView, naviBar, anoterButton, cardView, shareButton, resultButton])
+        
+        backView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        naviBar.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(49)
+            make.directionalHorizontalEdges.equalToSuperview()
+        }
+        
+        anoterButton.snp.makeConstraints { make in
+            make.top.equalTo(naviBar.snp.bottom).offset(5)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(MatrixLiteral.buttonSize)
+        }
+        
+        cardView.snp.makeConstraints { make in
+            make.top.equalTo(anoterButton.snp.bottom).offset(15)
+            make.directionalHorizontalEdges.equalToSuperview().inset(38)
+            make.height.equalTo(455)
+        }
+        
+        shareButton.snp.makeConstraints { make in
+            make.top.equalTo(cardView.snp.bottom).offset(47)
+            make.leading.equalTo(cardView.snp.leading)
+        }
+        
+        resultButton.snp.makeConstraints { make in
+            make.top.equalTo(cardView.snp.bottom).offset(47)
+            make.trailing.equalTo(cardView.snp.trailing)
+        }
     }
 }
