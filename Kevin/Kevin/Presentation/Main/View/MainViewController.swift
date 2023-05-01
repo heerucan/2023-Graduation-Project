4ff0ae9d8bc51e2bd7866b654cce0b532245885d
heerucan <iwpow0307@gmail.com>
2023-05-01 19:28:44 +0900

    add: FSCalendar 사용해 메인뷰 캘린더 커스텀 진행 #5 - 남은 것: 각 셀, 화면전환 디테일들

--- Kevin/Kevin/Presentation/Main/View/MainViewController.swift
+++ Kevin/Kevin/Presentation/Main/View/MainViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+import FSCalendar
 import RxSwift
 import RxCocoa
 import SnapKit
@@ -16,6 +17,17 @@ final class MainViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     private let viewModel: MainViewModel
+    
+    private let naviBar = KevinNavigationBar(type: .main)
+    
+    private let topTitleLabel = UILabel().then {
+        $0.text = StringLiteral.main
+        $0.font = .kevinFont(type: .medium22)
+        $0.textColor = .black
+        $0.numberOfLines = 2
+    }
+    
+    private let calendar = FSCalendar()
     
     init(viewModel: MainViewModel) {
         self.viewModel = viewModel
@@ -30,20 +42,95 @@ final class MainViewController: UIViewController {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setCalendarUI()
         bind()
     }
     
     private func bind() {
+        //        calendar.rx.
         
+    }
+}
+
+extension MainViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+//    func minimumDate(for calendar: FSCalendar) -> Date {
+//        return Date()
+//    }
+    
+    func maximumDate(for calendar: FSCalendar) -> Date {
+        return Date()
+    }
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendar.snp.updateConstraints { (make) in
+            make.height.equalTo(bounds.height)
+        }
+        calendar.layoutSubviews()
+    }
+    
+    /// 날짜 선택 시 화면전환
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("선택한 날짜", date)
+        calendar.setCurrentPage(date, animated: true)
+        calendar.appearance.titleFont = UIFont.systemFont(ofSize: 16, weight: .black)
+    }
+    
+    private func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleTodayFontFor date: Date) -> UIFont? {
+        /// 오늘 날짜의 폰트 크기 변경
+        return UIFont.systemFont(ofSize: 16, weight: .black)
     }
 }
 
 extension MainViewController {
     private func setUI() {
-        view.backgroundColor = .pink100
+        view.backgroundColor = .white
     }
     
     private func setLayout() {
+        view.addSubviews([naviBar, topTitleLabel, calendar])
         
+        naviBar.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(49)
+            make.directionalHorizontalEdges.equalToSuperview()
+        }
+        
+        topTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(naviBar.snp.bottom).offset(2)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        calendar.snp.makeConstraints { make in
+            make.top.equalTo(topTitleLabel.snp.bottom).offset(20)
+            make.directionalHorizontalEdges.equalToSuperview()
+            make.height.equalTo(330)
+        }
+    }
+    
+    private func setCalendarUI() {
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        calendar.layoutIfNeeded()
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.headerHeight = 60
+        calendar.weekdayHeight = 45
+        calendar.placeholderType = .none
+        calendar.allowsSelection = true
+        calendar.allowsMultipleSelection = false
+
+        calendar.appearance.headerDateFormat = "YYYY년 M월"
+        calendar.appearance.headerTitleOffset = CGPoint(x: -80, y: 0)
+        calendar.appearance.headerTitleAlignment = .left
+        calendar.appearance.headerTitleFont = .kevinFont(type: .medium16)
+        calendar.appearance.weekdayFont = .kevinFont(type: .medium12)
+        calendar.appearance.titleFont = .kevinFont(type: .regular16)
+        calendar.appearance.headerTitleColor = .black
+        calendar.appearance.weekdayTextColor = .black
+        calendar.appearance.titleDefaultColor = .black
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
+//        calendar.appearance.selectionColor = .white
+        
+        
+//        calendar.allowsSelection = true
     }
 }
