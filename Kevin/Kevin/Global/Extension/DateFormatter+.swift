dbf17a45e9f655b57b1e00ecba0871e5370edf11
heerucan <iwpow0307@gmail.com>
2023-05-17 05:17:16 +0900

    add: DateFormatterUtil 추가 #12

--- Kevin/Kevin/Global/Extension/DateFormatter+.swift
+++ Kevin/Kevin/Global/Extension/DateFormatter+.swift
@@ -1,8 +1,17 @@
 //
-//  DateFormatter+.swift
+//  DateFormatterUtil+.swift
 //  Kevin
 //
 //  Created by heerucan on 2023/05/17.
 //
 
-import Foundation
+import UIKit
+
+final class DateFormatterUtil {
+    static func formateDate(_ date: Date?) -> String {
+        guard let date = date else { return "" }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = StringLiteral.Calendar.fullDateFormat
+        return dateFormatter.string(from: date)
+    }
+}
