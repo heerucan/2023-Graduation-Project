f090de90a2a96ade317f05f886274d24cd66fc70
heerucan <iwpow0307@gmail.com>
2023-05-21 04:03:19 +0900

    add: viewWillAppear RxExtension 추가 #20

--- Kevin/Kevin/Global/Extension/Rx+.swift
+++ Kevin/Kevin/Global/Extension/Rx+.swift
@@ -8,4 +8,17 @@
 import UIKit
 
 import RxSwift
+import RxCocoa
 
+extension Reactive where Base: UIViewController {
+    
+    var viewDidLoad: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+        return ControlEvent(events: source)
+    }
+    
+    var viewWillAppear: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear)).map { _ in }
+        return ControlEvent(events: source)
+    }
+}
