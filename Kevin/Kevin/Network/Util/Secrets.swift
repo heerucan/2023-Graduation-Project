5537bdae718da791bb49c876cb367cdf1613957e
heerucan <iwpow0307@gmail.com>
2023-05-21 22:37:10 +0900

    update: .gitignore - secrets.json

---
+++ Kevin/Kevin/Network/Util/Secrets.swift
@@ -0,0 +1,8 @@
+//
+//  Secrets.swift
+//  Kevin
+//
+//  Created by heerucan on 2023/05/21.
+//
+
+import Foundation
---
+++ Kevin/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  Kevin
+#
+#  Created by heerucan on 2023/05/21.
+#  
