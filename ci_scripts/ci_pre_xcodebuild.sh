e513e6160f81543bac2c0d34e929585703a792c6
heerucan <iwpow0307@gmail.com>
2023-05-21 23:25:22 +0900

    chore: Secrets.json 경로 변경 #25

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -13,7 +13,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 cd $CI_WORKSPACE/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
-printf "{\"NAVER_KEY\":\"%s\",\"NAVER_KEY_ID\":\"%s\",\"CHAT_KEY\":\"%s\"}" "$NAVER_KEY" "$NAVER_KEY_ID" "$CHAT_KEY" >> ../Kevin/Application/Secrets.json
+printf "{\"NAVER_KEY\":\"%s\",\"NAVER_KEY_ID\":\"%s\",\"CHAT_KEY\":\"%s\"}" "$NAVER_KEY" "$NAVER_KEY_ID" "$CHAT_KEY" >> ../Kevin/Kevin/Application/Secrets.json
 
 echo "Wrote Secrets.json file."
 
